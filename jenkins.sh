a51cbfc3c99322b2b6bdb084b8a45c062e9f4d07
root <samson.mei@sap.com>
2018-04-13 18:11:16 +0800

    20180413181116

--- jenkins.sh
+++ jenkins.sh
@@ -3,8 +3,8 @@
 docker build -t test .
 docker rm -f test
 #docker run -itd --name test -p 5001:22 -p 5000:80 -v /volume:/share test
-docker run -itd --name test -p 5001:22 -p 5000:80 \
+docker run -itd --name test -p 5001:22 -p 5000:80  -v /volume2:/share test
 # -v $WORKSPACE/etc:/usr/local/nagios/etc \
 # -v /volume/nagios/var:/usr/local/nagios/var \
- -v /volume2:/share \
- test
+# -v /volume2:/share \
+# test
