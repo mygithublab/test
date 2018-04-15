e1996b3499991d6307787e7b1dd92d714356d076
mygithublab <mygithublab@126.com>
2018-04-15 21:10:12 +0800

    20180415211012

--- jenkins.sh
+++ jenkins.sh
@@ -2,5 +2,5 @@
 
 docker build -t test .
 docker rm -f test
-#docker run -itd --name test -p 5001:22 -p 5000:80 -v /volume2:/share --restart=always test
-docker run -itd --name test -p 5001:22 -p 5000:80 -v $WORKSPACE/resource/nagios/etc:/usr/local/nagios/etc -v /volume2/nagios/var:/usr/local/nagios/var -v /volume2/nagiosgraph/var:/usr/local/nagiosgraph/var -v /volume2:/share test
+docker run -itd --name test -p 5001:22 -p 5000:80 -v /volume2:/share --restart=always test
+#docker run -itd --name test -p 5001:22 -p 5000:80 -v $WORKSPACE/resource/nagios/etc:/usr/local/nagios/etc -v /volume2/nagios/var:/usr/local/nagios/var -v /volume2/nagiosgraph/var:/usr/local/nagiosgraph/var -v /volume2:/share test
