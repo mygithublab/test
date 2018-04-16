5e6db00fadf79961ec2da61d6c685a59b05c7e8c
root <samson.mei@sap.com>
2018-04-16 17:50:45 +0800

    20180416175045

--- jenkins.sh
+++ jenkins.sh
@@ -5,7 +5,7 @@ docker rm -f test
 #docker run -itd --name test -p 5001:22 -p 5000:80  -v /volume2:/share --restart=always test
 
 docker run -itd --name test -p 5001:22 -p 5000:80 \
- -v $WORKSPACE/resource/nagios/etc:/usr/local/nagios/etc \
+ -v $WORKSPACE/etc:/usr/local/nagios/etc \
  -v /volume/nagios/var:/usr/local/nagios/var \
  -v /volume/nagiosgraph/var:/usr/local/nagiosgraph/var \
  -v /volume2:/share \
