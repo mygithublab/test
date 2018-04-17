f5b7b0c9bc6502e1167cb60d5ce75f21f57789c0
root <samson.mei@sap.com>
2018-04-17 14:16:56 +0800

    20180417141656

--- jenkins.sh
+++ jenkins.sh
@@ -4,7 +4,7 @@ docker build -t external .
 #docker rm -f external
 #docker run -itd --name test -p 5001:22 -p 5000:80  -v /volume2:/share --restart=always test
 
-docker run -itd --name external -p 10000:22 -p 10001:80 \
+docker run -itd --name external -p 10001:22 -p 10000:80 \
  -v $WORKSPACE/etc:/usr/local/nagios/etc \
  -v /volume3/nagios/var:/usr/local/nagios/var \
  -v /volume3/nagiosgraph/var:/usr/local/nagiosgraph/var \
