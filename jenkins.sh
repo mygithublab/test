6243858180be58e85e91e6080aa99540952c213e
root <samson.mei@sap.com>
2018-04-17 14:17:52 +0800

    20180417141752

--- jenkins.sh
+++ jenkins.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 docker build -t external .
-#docker rm -f external
+docker rm -f external
 #docker run -itd --name test -p 5001:22 -p 5000:80  -v /volume2:/share --restart=always test
 
 docker run -itd --name external -p 10001:22 -p 10000:80 \
