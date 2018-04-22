0b037551a45de42dfb9ed0218a80577f9a166a40
mygithublab <mygithublab@126.com>
2018-04-22 12:49:45 +0800

    20180422124945

--- jenkins.sh
+++ jenkins.sh
@@ -2,11 +2,11 @@
 
 docker build -t external .
 docker rm -f external
-docker run -itd --name external -p 10001:22 -p 10000:80  -v /volume2:/share --restart=always external
+#docker run -itd --name external -p 10001:22 -p 10000:80  -v /volume2:/share --restart=always external
 
-#docker run -itd --name external -p 10001:22 -p 10000:80 \
-# -v $WORKSPACE/etc:/usr/local/nagios/etc \
-# -v /volume3/nagios/var:/usr/local/nagios/var \
-# -v /volume3/nagiosgraph/var:/usr/local/nagiosgraph/var \
-# -v /volume2:/share \
-# --restart=always external
+docker run -itd --name external -p 10001:22 -p 10000:80 \
+ -v $WORKSPACE/etc:/usr/local/nagios/etc \
+ -v /volume3/nagios/var:/usr/local/nagios/var \
+ -v /volume3/nagiosgraph/var:/usr/local/nagiosgraph/var \
+ -v /volume2:/share \
+ --restart=always external
