f162bf9f89fd601cdaca68a8436bdcb10bc5e428
Trial <mygithublab@126.com>
2018-11-21 11:35:25 +0800

    20181121113525

--- jenkins.sh
+++ jenkins.sh
@@ -4,9 +4,9 @@ docker build -t github-test-ubuntu-nagios-image .
 docker rm -f github-test-ubuntu-nagios-container
 #docker run -itd --name github-test-ubuntu-nagios-container -p 10001:22 -p 10000:80  -v /volume2:/share --restart=always github-test-ubuntu-nagios-image
 
-docker run -itd --name github-test-ubuntu-nagios-container -p 10086:80 -p 10087:22 \
+docker run -itd --name github-test-ubuntu-nagios-container -p 10086:80 -p 10087:22 -v /test:/share --restart=always github-test-ubuntu-nagios-image
 # -v $WORKSPACE/etc:/usr/local/nagios/etc \
 # -v /github/test/nagios/var:/usr/local/nagios/var \
 # -v /github/test/nagiosgraph/var:/usr/local/nagiosgraph/var \
- -v /test:/share \
- --restart=always github-test-ubuntu-nagios-image
+# -v /test:/share \
+# --restart=always github-test-ubuntu-nagios-image
